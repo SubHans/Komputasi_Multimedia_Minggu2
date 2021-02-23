084ce47eed902628eab4cce416bce1809eed10c7
SubHans <56150593+SubHans@users.noreply.github.com>
2021-02-23 20:30:38 +0700

    tugas2

---
+++ Assets/SliderValueToText2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValueToText2 : MonoBehaviour
+{
+    public Slider sliderUI;
+    private Text textSliderValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textSliderValue = GetComponent<Text>();
+        ShowSliderValue();
+    }
+
+    // Update is called once per frame
+    public void ShowSliderValue()
+    {
+        string sliderMessage = "Detilk = " +
+        sliderUI.value;
+        textSliderValue.text = sliderMessage;
+    }
+}
